da2a808ebeb20bc1b7f488f0d0ee2ae91138caf4
grunkleij <indrajithpg10@gmail.com>
2025-08-26 18:26:24 +0530

    first commit

---
+++ app/src/main/java/com/example/bruh/BottomManager.java
@@ -0,0 +1,32 @@
+package com.example.bruh;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomManager {
+
+    void setit(Activity activity, int selectedItemId){
+
+        BottomNavigationView btm = activity.findViewById(R.id.bottom_navigation);
+        btm.setSelectedItemId(selectedItemId);
+        btm.setOnItemSelectedListener(item ->{
+        int id = item.getItemId();
+        if(id == R.id.item_1){
+            Intent intent = new Intent(activity, MainActivity.class);
+            activity.startActivity(intent);
+            activity.overridePendingTransition(0, 0);
+            return true;
+        }
+        else if(id == R.id.item_2){
+            Intent intent = new Intent(activity, MainActivity2.class);
+            activity.startActivity(intent);
+            activity.overridePendingTransition(0, 0);
+            return true;
+        }
+        return true;
+    });
+    }
+
+}
---
+++ app/src/main/java/com/example/bruh/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.bruh;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        BottomManager btmmg = new BottomManager();
+        btmmg.setit(this, R.id.item_1);
+
+    }
+}
